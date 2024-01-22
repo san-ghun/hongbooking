d8bab2c3a9f044d82eef58ba58899e2494ccbd32
Hongbae Kim <hongbaki@c4c4c3.42berlin.de>
2024-01-22 10:17:58 +0100

    updated baseURL

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Example JavaScript for interactivity (optional)
-document.addEventListener('DOMContentLoaded', function () 
-{
+document.addEventListener('DOMContentLoaded', function () {
     // You can add JavaScript functionality here
     console.log('Website loaded successfully.');
+
+    // Get the base URL dynamically
+    var baseURL = 'https://HONGBAEKIM.github.io' + '/eval_booker';
+
+    // Log the dynamically generated URL to the console
+    console.log('Website URL:', baseURL);
 });
+
+
+//https:HONGBAEKIM.github.io/eval_booker
