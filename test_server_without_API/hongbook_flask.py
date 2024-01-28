b02fdcf26134bb6f1a60ea3fc4d1b33895ffcacd
Hongbae Kim <hongbaki@c4c3c3.42berlin.de>
2024-01-28 13:10:19 +0100

    feature/adding:location_of_firefox

--- test_server_without_API/hongbook_flask.py
+++ test_server_without_API/hongbook_flask.py
@@ -93,10 +93,18 @@ def handle_form():
     # localhost_number = random.randint(65536, 79999)
     # chrome_options.add_experimental_option("debuggerAddress", f"localhost:{localhost_number}")
 
-    firefox_options = FirefoxOptions()
-    firefox_options.binary_location = '/usr/bin/firefox'
-    driver = Firefox(options=firefox_options)
+    # firefox_options = FirefoxOptions()
+    # firefox_options.binary_location = '/usr/bin/firefox'
+    # driver = Firefox(options=firefox_options)
     
+    firefox_options = FirefoxOptions()
+
+    # Specify the path to the geckodriver executable.
+    geckodriver_path = '/home/ubuntu/hongbook2/test_server_without_API/geckodriver'
+    driver = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
+
+
+
     login_url = "https://auth.42.fr/auth/realms/students-42/protocol/openid-connect/auth?client_id=intra&redirect_uri=https%3A%2F%2Fprofile.intra.42.fr%2Fusers%2Fauth%2Fkeycloak_student%2Fcallback&response_type=code&state=e510170b7adc7ed8fc39319b0c9896692df12a594087df4c"
 
     driver.get(login_url)
