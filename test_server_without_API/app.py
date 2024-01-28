31590bab2e523ffa5191f1c89011b406a94586a6
Hongbae Kim <hongbaki@c4c3c3.42berlin.de>
2024-01-28 17:01:26 +0100

    feature/adding:port5000

--- test_server_without_API/app.py
+++ test_server_without_API/app.py
@@ -471,9 +471,9 @@ def handle_form():
     # return response
     return "Sucessed"
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
