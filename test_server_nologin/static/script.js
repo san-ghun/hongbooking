316bfb7d19ea2e4c7378b633484bd7716a084120
Hongbae Kim <hongbaki@c4c2c4.42berlin.de>
2024-01-31 11:56:40 +0100

    feature/deleting:files

--- test_server_nologin/static/script.js
+++
@@ -1,6 +0,0 @@
-// Example JavaScript for interactivity (optional)
-document.addEventListener('DOMContentLoaded', function () 
-{
-    // You can add JavaScript functionality here
-    console.log('Website loaded successfully.');
-});
